87db193bbeb8588127633389caf5fc210df3979b
Ryan Hollins <ballbaroi@gmail.com>
2020-02-26 14:01:09 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,22 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    "For glory.".end_with?(".")
+    "Hi, my name is Sophie".end_with?(".")
     
   end
 
   def question?
-    "Where is le beef?".end_with?("?")
-    "That is a pear.".end_with?("?")
+    "What's your name?".end_with?("?")
 
   end
 
   def exclamation?
-    "YOU OUUGHT NOT HAVE DONE THAT!".end_with?("!")
+    "Hi, my name is Sophie".end_with?("!")
 
   end
 
   def count_sentences
+    "one. two. three?"
+    "0"
 
   end
 end
