b419dd2123810490a8a66e972a9cfab1370aed27
Ryan Hollins <ballbaroi@gmail.com>
2020-02-26 14:09:08 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,14 +3,17 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(.)
+    if self.end_with?(".")
       return true
     else
       return false
   end
 
   def question?
-    if self.end_with?(?)
+    if self.end_with?("?")
+      return true
+    else
+      return false
   end
 
   def exclamation?
